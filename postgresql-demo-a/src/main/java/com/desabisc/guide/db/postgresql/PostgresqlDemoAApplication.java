03ca2180807c2e7666fb1b81e8c53d545691dcee
desabi <desabi@live.com>
2025-04-02 16:17:00 -0600

    feat: postgresql-demo-a, base project

---
+++ postgresql-demo-a/src/main/java/com/desabisc/guide/db/postgresql/PostgresqlDemoAApplication.java
@@ -0,0 +1,13 @@
+package com.desabisc.guide.db.postgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresqlDemoAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresqlDemoAApplication.class, args);
+	}
+
+}
---
+++ postgresql-demo-a/src/test/java/com/desabisc/guide/db/postgresql/PostgresqlDemoAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desabisc.guide.db.postgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresqlDemoAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
