0050c44f544220d8fd3f29db16199ffec54906a1
desabi <desabi@live.com>
2025-10-17 11:28:47 -0600

    feat: h2-ega, base project

---
+++ crud-h2-ega/src/main/java/com/desabi/guide/crud/h2/ega/CrudH2EgaApplication.java
@@ -0,0 +1,13 @@
+package com.desabi.guide.crud.h2.ega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudH2EgaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudH2EgaApplication.class, args);
+	}
+
+}
---
+++ crud-h2-ega/src/test/java/com/desabi/guide/crud/h2/ega/CrudH2EgaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desabi.guide.crud.h2.ega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudH2EgaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
