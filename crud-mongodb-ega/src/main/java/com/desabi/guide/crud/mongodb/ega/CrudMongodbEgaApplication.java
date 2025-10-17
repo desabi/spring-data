0f39feae5823047b5d96d6ca64c543a49cff0006
desabi <desabi@live.com>
2025-10-17 11:54:53 -0600

    feat: mongo ega, base project

---
+++ crud-mongodb-ega/src/main/java/com/desabi/guide/crud/mongodb/ega/CrudMongodbEgaApplication.java
@@ -0,0 +1,13 @@
+package com.desabi.guide.crud.mongodb.ega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudMongodbEgaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudMongodbEgaApplication.class, args);
+	}
+
+}
---
+++ crud-mongodb-ega/src/test/java/com/desabi/guide/crud/mongodb/ega/CrudMongodbEgaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desabi.guide.crud.mongodb.ega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudMongodbEgaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
