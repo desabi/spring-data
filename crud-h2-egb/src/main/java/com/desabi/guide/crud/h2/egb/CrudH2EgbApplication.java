5cb70b252e64d7eed60d2194e9db1a63e59b7527
desabi <desabi@live.com>
2025-10-19 19:49:53 -0600

    feat: h2-egb, create

---
+++ crud-h2-egb/src/main/java/com/desabi/guide/crud/h2/egb/CrudH2EgbApplication.java
@@ -0,0 +1,13 @@
+package com.desabi.guide.crud.h2.egb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudH2EgbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudH2EgbApplication.class, args);
+	}
+
+}
---
+++ crud-h2-egb/src/test/java/com/desabi/guide/crud/h2/egb/CrudH2EgbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desabi.guide.crud.h2.egb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudH2EgbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
